0187e8219521c3f95fafe7813b1aee2b75ad0caf
Deb Pvt Ltd <debasish.pradhan@gmail.com>
2025-10-02 16:32:10 +0530

    test_transpose.py updated for data structure

--- tests/test_transpose.py
+++ tests/test_transpose.py
@@ -6,7 +6,26 @@ from maths.transpose import numpy_transpose
 
 
 @pytest.mark.parametrize("vector, exp_transpose",
-                         [([[1], [2], [3], [4]], [[1, 2, 3, 4]]), ([[5, 6, 7, 8]], [[5], [6], [7], [8]])])
+                         [([[1], [2], [3], [4]], [[1, 2, 3, 4]]), ([[5, 6, 7, 8]], [[5], [6], [7], [8]]),
+                          ([
+                               [1, 2],
+                               [3, 4]
+                           ],
+                           [
+                               [1, 3],
+                               [2, 4]
+                           ]),
+                          ([
+                               [1, 2, 3],
+                               [3, 4, 5],
+                               [5, 6, 7],
+                               [7, 8, 11]
+                           ],
+                           [
+                               [1, 3, 5, 7],
+                               [2, 4, 6, 8],
+                               [3, 5, 7, 11]
+                           ])])
 def test_numpy_transpose_vector(vector, exp_transpose):
     vector = numpy.array(vector)
     print(f'Vector - \n{vector}')
