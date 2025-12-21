51c2fe104380d3ee3cbe3a1c58e41954b701cd27
debasish pradhan <debasish.pradhan@gmail.com>
2025-12-22 00:43:41 +0530

    updated test_ai_model.py

--- tests/llm/test_ai_model.py
+++ tests/llm/test_ai_model.py
@@ -1,11 +1,9 @@
-import json
-
 from src.llm.AIModel import AIModel
 from src.llm.OpenAIModel import OpenAIModel
 
 
 def test_model_response():
-    ai_model: AIModel = OpenAIModel(model_name="gpt-oss-120b")
+    ai_model: AIModel = OpenAIModel(model_name="gpt-4.1-mini")
     response = ai_model.execute(
         prompt="Write a one-sentence bedtime story about a unicorn" )
-    assert isinstance(json.loads(response), dict )
+    assert isinstance(response, str )
