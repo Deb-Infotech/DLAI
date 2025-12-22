c46a4df5b177ad43d9e28db3c7da48c5087a9fdc
dpradhan79 <debasish.pradhan@gmail.com>
2025-12-22 20:31:12 +0530

    Add .env file for OpenAI API key and update OpenAIModel to load environment variables

--- src/llm/OpenAIModel.py
+++ src/llm/OpenAIModel.py
@@ -8,15 +8,17 @@ from src.llm.AIModel import AIModel
 
 class OpenAIModel(AIModel):
     def __init__(self, model_name: str):
+        # Load environment variables from a .env file
         dotenv.load_dotenv()
-        api_key = os.getenv("OPENAI_API_KEY")
-        self.client = OpenAI(api_key=api_key)
+        # by default, the OpenAI library reads the API key from the OPENAI_API_KEY environment variable
+        #api_key = os.getenv("OPENAI_API_KEY")
+        self.client = OpenAI()
         super().__init__(model_name=model_name)
 
     def execute(self, prompt: str) -> str:
         response = self.client.responses.create(
             model=self.model_name,
-            input="Write a one-sentence bedtime story about a unicorn."
+            input="who is prime minister of India?"
         )
 
         return response.output_text
